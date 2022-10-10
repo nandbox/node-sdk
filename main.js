156032f6e0f7cc41bc8f4ea73bf27393fe05abf0
samydevv <Samy.Hafeiz97@eng-st.cu.edu.eg>
2022-10-10 16:45:23 +0200

    Test the dependencies update
    successfully run the main after the dependencies update

--- main.js
+++ main.js
@@ -3,12 +3,9 @@ const NandBox = require("./src/NandBox");
 const Nand = require("./src/NandBoxClient");
 const NandBoxClient = Nand.NandBoxClient;
 
-const Logger = require('./src/util/Logger');
-
-
+const Logger = require("./src/util/Logger");
 
 const config = require("./config.json");
-
 
 var client = NandBoxClient.get(config);
 var nandbox = new NandBox();
@@ -16,52 +13,51 @@ var nCallBack = nandbox.Callback;
 var api = null;
 
 nCallBack.onConnect = (_api) => {
-    // it will go here if the bot connected to the server successfuly 
-    api = _api;
-    console.log("Authenticated");
-    Logger.logger.info("Authenticated");
-}
+  // it will go here if the bot connected to the server successfuly
+  api = _api;
+  console.log("Authenticated");
+  Logger.logger.info("Authenticated");
+};
 
+nCallBack.onReceive = (incomingMsg) => {
+  console.log("Message Received");
+  Logger.logger.info("Message Received");
 
-nCallBack.onReceive = incomingMsg => {
-    console.log("Message Received");
-    Logger.logger.info("Message Received");
-
-    if (incomingMsg.isTextMsg()) {
-        let chatId = incomingMsg.chat.id; // get your chat Id
-        let text = incomingMsg.text; // get your text message
-        api.sendText(chatId, text); // Sending message back as an Echo
-    }
-
-}
+  if (incomingMsg.isTextMsg()) {
+    let chatId = incomingMsg.chat.id; // get your chat Id
+    let text = incomingMsg.text; // get your text message
+    api.sendText(chatId, text); // Sending message back as an Echo
+  }
+};
 
 // implement other nandbox.Callback() as per your bot need
-nCallBack.onReceiveObj = obj => {
-    console.log("received object: ", obj);
-    Logger.logger.info("received object: " + JSON.stringify(obj));
-}
+nCallBack.onReceiveObj = (obj) => {
+  console.log("received object: ", obj);
+  Logger.logger.info("received object: " + JSON.stringify(obj));
+};
 
 nCallBack.onClose = () => {
-    Logger.logger.info("ONCLOSE");
-}
-nCallBack.onError = () => {  
-    Logger.logger.error("ONERROR");   
-}
-nCallBack.onChatMenuCallBack = chatMenuCallback => { }
-nCallBack.onInlineMessageCallback = inlineMsgCallback => { }
-nCallBack.onMessagAckCallback = msgAck => { }
-nCallBack.onUserJoinedBot = user => { }
-nCallBack.onChatMember = chatMember => { }
-nCallBack.onChatAdministrators = chatAdministrators => { }
-nCallBack.userStartedBot = user => { }
-nCallBack.onMyProfile = user => { }
-nCallBack.onUserDetails = user => { }
-nCallBack.userStoppedBot = user => { }
-nCallBack.userLeftBot = user => { }
-nCallBack.permanentUrl = permenantUrl => { }
-nCallBack.onChatDetails = chat => { }
-nCallBack.onInlineSearh = inlineSearch => { }
-
-
+  Logger.logger.info("ONCLOSE");
+};
+nCallBack.onError = () => {
+  Logger.logger.error("ONERROR");
+};
+nCallBack.onChatMenuCallBack = (chatMenuCallback) => {
+  console.log("Inside onChatMenuCallBack");
+  console.log("chatMenuCallback: ", chatMenuCallback);
+};
+nCallBack.onInlineMessageCallback = (inlineMsgCallback) => {};
+nCallBack.onMessagAckCallback = (msgAck) => {};
+nCallBack.onUserJoinedBot = (user) => {};
+nCallBack.onChatMember = (chatMember) => {};
+nCallBack.onChatAdministrators = (chatAdministrators) => {};
+nCallBack.userStartedBot = (user) => {};
+nCallBack.onMyProfile = (user) => {};
+nCallBack.onUserDetails = (user) => {};
+nCallBack.userStoppedBot = (user) => {};
+nCallBack.userLeftBot = (user) => {};
+nCallBack.permanentUrl = (permenantUrl) => {};
+nCallBack.onChatDetails = (chat) => {};
+nCallBack.onInlineSearh = (inlineSearch) => {};
 
 client.connect(config.Token, nCallBack);
